c21d411381b70674c6374f78f95ea532875d7da0
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-12-19 16:59:52 +0200

    FEAT: Created a product page

---
+++ components/Product.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+type Props = {
+  product: Product;
+};
+
+const Product = ({ product }: Props) => {
+  return (
+    <Link href={`/product/${product.id}`}>
+      <div>
+        <p>{product.title}</p>
+        <p>{product.price}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Product;
