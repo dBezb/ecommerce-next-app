7cbe9bffe7a9f2bddc1511f636e2ece846bb1278
Dmytro Bezborodko <bezborodkodima12@gmail.com>
2023-12-19 17:01:14 +0200

    FEAT: Get the API

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-export default function Home() {
+import Product from "@/components/Product";
+
+export default async function Home() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const product: Product = await res.json();
+
   return (
     <main className="">
       <section className="flex flex-col space-y-12 pb-44">
-        <h1 className="text-5x1 font-bold text-center">Deals of the day</h1>
+        <h1 className="text-5xl font-bold text-center">Deals of the day</h1>
+        {product.map((product: Product) => (
+          <Product key={product.id} product={product} />
+        ))}
       </section>
     </main>
   );
